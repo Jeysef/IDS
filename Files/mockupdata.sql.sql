7d3a46d703552334e5f720d86369e52583cf618e
1D3D4RY <IDEDARY@gmail.com>
2026-04-07 15:38:58 +0200

    feat: List products and their status

---
+++ Files/mockupdata.sql.sql
@@ -0,0 +1,7 @@
+INSERT INTO PRODUCTS (NAME, DESCRIPTION, CURRENT_PRICE, QUANTITY, STATUS_ID) VALUES (
+    'Epstein Klávesnice',
+    'Exotické periferie pro nejmocnejší hráče',
+    999,
+    1,
+    1
+)
